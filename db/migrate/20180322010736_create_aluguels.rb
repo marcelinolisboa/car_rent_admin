a811f1aa66ba6d9a4ea1712f5bf34df036b78424
Marcelino <marcel.lisboa@uol.com.br>
2018-03-28 22:50:29 +0000

    Criando projeto car_rental

---
+++ db/migrate/20180322010736_create_aluguels.rb
@@ -0,0 +1,15 @@
+class CreateAluguels < ActiveRecord::Migration[5.1]
+  def change
+    create_table :aluguels do |t|
+      t.decimal :preco, :decimal, :precision=>8, :scale=> 2
+      t.boolean :status
+      t.datetime :data_retirada
+      t.datetime :data_devolucao
+      t.integer :carro_id
+      t.string :cliente
+      t.integer :id
+
+      t.timestamps
+    end
+  end
+end
